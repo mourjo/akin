5be2c24a0bc3b345331931df8572b459e17c1f07
Mourjo Sen <hello@mourjo.me>
2021-07-25 09:51:49 +0530

    Tests for matcher

--- src/test/java/me/mourjo/MatcherTest.java
+++ src/test/java/me/mourjo/MatcherTest.java
@@ -20,23 +20,23 @@ import org.junit.jupiter.api.Test;
 class MatcherTest {
 
   static Matcher matcher;
-  static List<Map<Row, Row>> matchSlices;
+  static List<Map<Row, Row>> matchSlices = new ArrayList<>();
 
-  static void fillTestMovieFile(File f, int lines) throws IOException {
+  static File createTestMovieFile(int lines) throws IOException {
+    File tempFile = File.createTempFile("akin_test_", ".tsv");
     try (BufferedReader reader = new BufferedReader(
         new FileReader("src/main/dev_resources/dedup-2020/movies.tsv"));
-        PrintWriter writer = new PrintWriter(new FileWriter(f.getAbsolutePath()))) {
+        PrintWriter writer = new PrintWriter(new FileWriter(tempFile.getAbsolutePath()))) {
       while (lines-- >= 0) {
         writer.println(reader.readLine());
       }
     }
+    return tempFile;
   }
 
   @BeforeAll
   public static void setup() throws IOException {
-    File tempFile = File.createTempFile("akin_test_", ".tsv");
-    fillTestMovieFile(tempFile, 100_000);
-    matchSlices = new ArrayList<>();
+    File tempFile = createTestMovieFile(100_000);
     matcher = new Matcher(tempFile.getAbsolutePath());
     var it = matcher.computeSliceMatches();
     while (it.hasNext()) {
